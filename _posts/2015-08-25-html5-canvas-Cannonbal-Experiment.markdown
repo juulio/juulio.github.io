fe134f301ee455c19827f665749c17e6192031be
Julio Del Valle <julio.delvalle@possible.com>
2015-09-09 17:45:45 -0600

    Adding cannonbal js for Canvas experiment

---
+++ _posts/2015-08-25-html5-canvas-Cannonbal-Experiment.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  "HTML5 Canvas - Cannonbal Experiment"
+date:   2015-09-09 17:14
+categories: post
+---
+<canvas id = "canvas" width = "640" height = "480" style="border:1px solid"> No HTML5 Canvas detected!
+</canvas>
+<script src="/js/canvasExperiments/cannonball.js"></script>
---
+++ js/global.js
@@ -0,0 +1,26 @@
+/***
+ * Julio Del Valle - Computer Graphics - Costa Rica - 2015
+ * juulio.github.io
+ */
+var homePage = homePage || {};
+
+(function (context) {
+
+    /***
+     * Global Variables
+     */
+    vars = {
+
+
+    };
+
+    /***
+     * Init all required functions
+     */
+    function init () {
+      console.log('This javascript code will be executed on all pages');
+    }
+
+    init();
+
+}(homePage));
