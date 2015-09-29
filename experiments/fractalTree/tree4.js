6f133964b1fdd47d4a3e90a116e844569d6c8352
Julio Del Valle <julio.delvalle@possible.com>
2015-09-28 18:12:44 -0600

    Fixing Fourth Fractal Tree

--- experiments/fractalTree/tree4.js
+++ experiments/fractalTree/tree4.js
@@ -1,27 +1,83 @@
-var canvas = document.getElementById('canvas'),
-    context = canvas.getContext('2d'),
-    treeDepth = 5,
-    rotationAngle = 10.0,
-    lineLength = 8,
-    angleOffset = 2,
-    startingPointX = canvas.width/2,
-    startingPointY = canvas.height-150;
+/***
+ * Julio Del Valle - Computer Graphics - Costa Rica - 2015
+ * Fractal Tree - juulio.github.io
+ */
+var fractalTree = fractalTree || {};
+
+(function (context) {
+
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        angleOffset = 2,
+        i = 0;
+
+    function random(min, max){
+		return min + Math.floor(Math.random()*(max+1-min));
+	}
+
+    function cos (angle) {
+        return Math.cos(deg_to_rad(angle));
+    }
+
+    function sin (angle) {
+        return Math.sin(deg_to_rad(angle));
+    }
+
+    function deg_to_rad(angle){
+        return angle*(Math.PI/180.0);
+    }
+
+    function drawLine(x1,y1,x2,y2) {
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+    }
+
+    var drawFourthTree = function(x1, y1, angle, treeDepth, lineLength){
+
+        if(treeDepth !=0) {
+
+            var x2 = x1 + (cos(angle) * treeDepth * lineLength),
+                y2 = y1 + (sin(angle) * treeDepth * lineLength);
+
+            treeDepth--;
+            lineLength*=0.8;
+
+            drawLine(x1, y1, x2, y2);
+
+            drawFourthTree(x2, y2, angle - random(15,20), treeDepth, lineLength);
+            drawFourthTree(x2, y2, angle + random(15,20), treeDepth, lineLength);
+        }
+    };
+
+    function init(){
+
+        drawFourthTree(canvas.width/2, canvas.height-150, -90, 13, 8);
+    }
+
+    init();
+
+}(fractalTree));
+
+
+
 
 
 //-----------------------------------------------------------------------------------------------------------------
 // function: drawTrunk: draws the trunk from the initial starting point. This is not part of the recursive process.
 //   drawTrunk(DEPTH, rotationAngle);
 //--------------------------------------------------------------
-(function drawTrunk() {
-    context.lineWidth = 6;
-    drawLine(startingPointX, startingPointY, startingPointX, canvas.height);
-    context.translate(startingPointX, startingPointY);
-    // //ofSetLineWidth(treeDepth*4);
-    // ofSetLineWidth(3);
-    // ofSetColor(92, 51, 23);
-    // ofLine(0,ofGetHeight(),0,0);
-    drawBranch(treeDepth, rotationAngle);
-}());
+// (function drawTrunk() {
+//     context.lineWidth = 6;
+//     drawLine(startingPointX, startingPointY, startingPointX, canvas.height);
+//     context.translate(startingPointX, startingPointY);
+//     // //ofSetLineWidth(treeDepth*4);
+//     // ofSetLineWidth(3);
+//     // ofSetColor(92, 51, 23);
+//     // ofLine(0,ofGetHeight(),0,0);
+//     drawBranch(treeDepth, rotationAngle);
+// }());
 
 //--------------------------------------------------------------
 function drawBranch(treeDepth, angle) {
