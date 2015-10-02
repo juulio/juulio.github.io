658065d0935fa2317cc3e3c06135624b99425e32
Julio Del Valle <julio.delvalle@possible.com>
2015-10-02 14:32:48 -0600

    Adding HTML and JS Canvas template

---
+++ experiments/canvasTemplate/canvasTemplate.js
@@ -0,0 +1,24 @@
+/*************************************
+ * Julio Del Valle - Costa Rica - 2015
+ * HTML 5 Canvas Code template
+ */
+
+// Create the Canvas Element
+var theCanvas = document.createElement("canvas");
+document.body.appendChild(theCanvas);
+document.body.style.margin = 0;
+
+// Apply basic styles to the Canvas Element
+theCanvas.style.width = '600px';
+theCanvas.style.height = '400px';
+theCanvas.style.border = 'solid 1px red';
+theCanvas.style.display = 'block';
+theCanvas.style.margin = '0 auto';
+
+var context = theCanvas.getContext("2d");
+
+function drawScreen() {
+
+}
+
+drawScreen();
