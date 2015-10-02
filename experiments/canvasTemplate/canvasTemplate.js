6882400514c8fb11d09a57ee498f8f85b5316b85
Julio Del Valle <julio.delvalle@possible.com>
2015-10-02 14:40:34 -0600

    Fixing Canvas template js

--- experiments/canvasTemplate/canvasTemplate.js
+++ experiments/canvasTemplate/canvasTemplate.js
@@ -3,22 +3,27 @@
  * HTML 5 Canvas Code template
  */
 
-// Create the Canvas Element
-var theCanvas = document.createElement("canvas");
-document.body.appendChild(theCanvas);
-document.body.style.margin = 0;
 
-// Apply basic styles to the Canvas Element
-theCanvas.style.width = '600px';
-theCanvas.style.height = '400px';
-theCanvas.style.border = 'solid 1px red';
-theCanvas.style.display = 'block';
-theCanvas.style.margin = '0 auto';
+// Main Canvas App Wrapper Function
+function canvasApp(){
 
-var context = theCanvas.getContext("2d");
+    // Create the Canvas Element
+    var theCanvas = document.createElement("canvas");
+    document.body.appendChild(theCanvas);
+    document.body.style.margin = 0;
 
-function drawScreen() {
+    // Apply basic styles to the Canvas Element
+    theCanvas.style.width = '600px';
+    theCanvas.style.height = '400px';
+    theCanvas.style.border = 'solid 1px red';
+    theCanvas.style.display = 'block';
+    theCanvas.style.margin = '0 auto';
 
+    var context = theCanvas.getContext("2d");
+
+    function drawScreen() {
+
+    }
+
+    drawScreen();
 }
-
-drawScreen();
