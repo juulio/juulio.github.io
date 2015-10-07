56f9b2c950c3113beea12a532c4c4b127cf96839
unknown <juulio@gmail.com>
2015-10-06 22:39:31 -0600

    Adding animated tree code. Not ready yet

--- experiments/fractalTree/tree.js
+++ experiments/fractalTree/tree.js
@@ -1,112 +1,97 @@
-window.onload = function(){
-    var canvas = document.getElementById("canvas"),
-        ctx = canvas.getContext("2d"),
-        windowWidth = window.innerWidth,
-        windowHeight = window.innerHeight;
+ /***
+ * Julio Del Valle - Computer Graphics - Costa Rica - 2015
+ * Fractals Forest - juulio.github.io
+ */
+var fractalTree = fractalTree || {};
 
-    canvas.width = windowWidth;
-    canvas.height = windowHeight;
+(function (context) {
 
-    var length, divergence, reduction, line_width, start_points = [];
+    // Initial Setup
+    var canvas = document.getElementById('canvas'),
+        context = canvas.getContext('2d'),
+        canvasHalfWidth = canvas.width/2,
+        fractalProportion = 0.8,
+        branchLength = 50,
+        angle = 30 * Math.PI / 180;
+    
+    // Helper function: draws a Line from (x1,y1) to (x2,y2)
+    function drawLine(x1, y1, x2, y2){
+        context.beginPath();
 
-    init();
+        context.moveTo(x1,y1);
+        context.lineTo(x2, y2);
+
+        context.closePath();
+        context.stroke();
+    }
+
+    // Helper function: draws a dot at (x,y) with a radius r
+    function drawDot(x,y,r) {
+        context.beginPath();
+        context.arc(x, y, r, 0, 2*Math.PI, false);
+        context.stroke();
+    }
+
+    // Helper function: draws a line from (x1,y1) to (x2,y2), animating dot by dot until it reaches (x2,y2)
+    function drawAnimatedLine(y1,y2) {
+        //http://stackoverflow.com/questions/9762877/html5-js-how-to-animate-straight-line-between-two-coordinates
+        console.log('hola');
+        for(y1=0;y1>y2;y1-=0.01){
+            drawLine(0,0,0,y1);
+        }
+    }
+
+    // Recursive Fractal Function
+    function drawTree(startX, startY, branchLength, angle, depth, lineWidth){
+        context.lineWidth = lineWidth;
+        context.translate(startX, startY);
+
+        // drawLine(0, 0, 0, -branchLength);
+        for(var i=0;i>-branchLength;i-=0.7){
+            drawLine(0, 0, 0, -branchLength);
+        }
+        // drawAnimatedLine(0, -branchLength);
+
+        //--------------------------------
+
+        if(depth > 0 ){
+            depth--;
+            context.translate(0, -branchLength);
+
+            angle += fractalProportion;
+            lineWidth *= fractalProportion;
+            branchLength *= fractalProportion;
+
+            context.save();
+
+            // Draw Right Branch
+            context.rotate(angle * Math.PI / 180);
+            //drawLine(x1, y1, x2, y2);
+            drawTree(0, 0, branchLength, angle, depth, lineWidth);
+
+
+            context.restore();
+            context.save();
+
+            // Draw Left Branch
+            context.rotate(-angle * Math.PI / 180);
+            //drawLine(x1, y1, x2, y2);
+            drawTree(0, 0, branchLength, angle, depth, lineWidth);
+            context.restore();
+        }
+    }
 
     function init(){
-        // Fill the canvas white
-        ctx.fillStyle = '#fff';
-        ctx.fillRect(0, 0, windowWidth, windowHeight);
+        var button = document.getElementsByClassName("fractalTreeButton");
+        button[0].onclick = function(){
+            // First clear the whole canvas area.
+            context.clearRect(0,0,canvas.width, canvas.height);
 
-        // Randomize the Trunk's length 100 - 150
-        length = 100 + Math.round(Math.random()*50);
-
-        // Randomize the angle at which branches will diverge 10 - 60
-        divergence = 10 + Math.round(Math.random()*50);
-
-        // Every branch will be 0.5 - 0.75 times shorter than the previous one, with two decimal points.
-        reduction = Math.round(50 + Math.random()*20)/100;
-
-        // Width of the branch/trunk
-        line_width = 10;
-
-        // This is the trunk's endpoint, from where branches will diverge
-        var trunk = {
-            x : windowWidth/2,
-            y : length+50,
-            angle : 90
-        }
-
-        // The trunk becomes the starting point for branches
-        start_points = []; // empty the start_points array on every init();
-        start_points.push(trunk);
-
-        ctx.beginPath();
-        ctx.moveTo(trunk.x, windowHeight-50);
-        ctx.lineTo(trunk.x, windowHeight-trunk.y);
-        ctx.strokeStyle = 'brown';
-        ctx.lineWidth = line_width;
-        ctx.stroke();
-
-        branches();
-    }
-
-    function branches(){
-        // Reduce line_width and length
-        length *= reduction;
-        line_width *= reduction;
-        ctx.lineWidth = line_width;
-
-        var new_start_points = [];
-        ctx.beginPath();
-
-        for(var i=0;i<start_points.length;i++){
-            var sp = start_points[i];
-            // 2 branches will come out of every start point, hence there will be 2 endpoints.
-            // There is a difference in the divergence.
-            var ep1 = get_endpoint(sp.x, sp.y, sp.angle+divergence, length);
-            var ep2 = get_endpoint(sp.x, sp.y, sp.angle-divergence, length);
-
-            // Draw the branches
-            ctx.moveTo(sp.x, windowHeight-sp.y);
-            ctx.lineTo(ep1.x, windowHeight-ep1.y);
-            ctx.moveTo(sp.x, windowHeight-sp.y);
-            ctx.lineTo(ep2.x, windowHeight-ep2.y);
-
-            ep1.angle = sp.angle+divergence;
-            ep2.angle = sp.angle-divergence;
-
-            new_start_points.push(ep1);
-            new_start_points.push(ep2);
-        }
-
-        if(length<10){
-            ctx.strokeStyle = 'green';
-        }
-        else{
-            ctx.strokeStyle = 'brown';
-        }
-
-        ctx.stroke();
-
-        start_points = new_start_points;
-
-        // if length > 2, do the recursive call
-        if(length>2){
-            setTimeout(branches, 50);
-        }
-        else{
-            setTimeout(init, 500);
+            drawTree(canvasHalfWidth, canvas.height, 70, 25, 1, 14);    
         }
     }
 
-    // This funciont calculates the end points based on vectors.
-    function get_endpoint(x, y, a, length) {
-        var epx = x + length * Math.cos(a*Math.PI/180);
-        var epy = y + length * Math.cos(a*Math.PI/180);
+    init();
+    
 
-        return {
-            x: epx,
-            y: epy
-        }
-    }
-
-}
+}(fractalTree));
