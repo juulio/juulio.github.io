e6c83920bee3468a8122fd2787a3e5be87901682
Julio Del Valle <julio.delvalle@possible.com>
2015-10-12 22:49:09 -0600

    Adding tetris folder. Adding backround grid

---
+++ experiments/tetris/tetrisUI.js
@@ -0,0 +1,95 @@
+/*************************************
+ * Julio Del Valle - Costa Rica - 2015
+ * HTML 5 Canvas TETRIS - User Interface
+ */
+
+// Main Canvas App Wrapper Function
+function canvasUI(){
+
+    // Create the Canvas Element
+    var theCanvas = document.createElement("canvas");
+    theCanvas.width = 400;
+    theCanvas.height = 800;
+    document.body.appendChild(theCanvas);
+    document.body.style.margin = 0;
+
+    // Apply basic styles to the Canvas Element
+    theCanvas.style.border = 'solid 1px red';
+    theCanvas.style.display = 'block';
+
+    var context = theCanvas.getContext("2d");
+
+    /***
+    * Draws a line from (x1,y1) to (x2,y2)
+    */
+    function drawLine(x1,y1,x2,y2) {
+        context.beginPath();
+        context.moveTo(x1,y1);
+        context.lineTo(x2,y2);
+        context.stroke();
+    }
+
+    /***
+    * Draws the background grid (10 x 20 blocks)
+    */
+    function drawGrid(){
+        context.strokeStyle = "#707070";
+
+        // Vertical Lines
+        drawLine(40,0,40,800);
+        drawLine(80,0,80,800);
+        drawLine(120,0,120,800);
+        drawLine(160,0,160,800);
+        drawLine(200,0,200,800);
+        drawLine(240,0,240,800);
+        drawLine(280,0,280,800);
+        drawLine(320,0,320,800);
+        drawLine(360,0,360,800);
+
+        // Horizontal Lines
+        drawLine(0,40,400,40);
+        drawLine(0,80,400,80);
+        drawLine(0,120,400,120);
+        drawLine(0,160,400,160);
+        drawLine(0,200,400,200);
+        drawLine(0,240,400,240);
+        drawLine(0,280,400,280);
+        drawLine(0,320,400,320);
+        drawLine(0,360,400,360);
+        drawLine(0,400,400,400);
+        drawLine(0,440,400,440);
+        drawLine(0,480,400,480);
+        drawLine(0,520,400,520);
+        drawLine(0,560,400,560);
+        drawLine(0,600,400,600);
+        drawLine(0,640,400,640);
+        drawLine(0,680,400,680);
+        drawLine(0,720,400,720);
+        drawLine(0,760,400,760);
+        drawLine(0,800,400,800);
+    }
+
+    function drawScreen() {
+        drawGrid();
+    }
+
+
+    window.requestAnimFrame = (function(){
+        return window.requestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            window.mozRequestAnimationFrame ||
+            window.oRequestAnimationFrame ||
+            window.msRequestAnimationFrame ||
+            function( callback ){
+                window.setTimeout(callback, 1000 / 60);
+            };
+    })();
+
+    // Code originally developed by Paul Irish)
+    (function animloop(){
+        requestAnimFrame(animloop);
+        drawScreen();
+    })();
+}
+
+window.onload = canvasUI;
