ce5874a0bb4d879a524d8591213a179e36ccb80a
Julio Del Valle <julio.delvalle@possible.com>
2015-10-12 23:18:28 -0600

    Adding initial drawing functions for square block

--- experiments/tetris/tetrisUI.js
+++ experiments/tetris/tetrisUI.js
@@ -19,6 +19,9 @@ function canvasUI(){
 
     var context = theCanvas.getContext("2d");
 
+    // Game Variables
+    var blocks = [];
+
     /***
     * Draws a line from (x1,y1) to (x2,y2)
     */
@@ -27,6 +30,16 @@ function canvasUI(){
         context.moveTo(x1,y1);
         context.lineTo(x2,y2);
         context.stroke();
+    }
+
+    /***
+    * Draws a square starting at (x,y) using lengh as width and height
+    */
+    function drawSquare(x,y,length) {
+        drawLine(x,y,x+length,y);
+        drawLine(x,y,x,y+length);
+        drawLine(x,y+length,x+length,y+length);
+        drawLine(x+length,y,x+length,y+length);
     }
 
     /***
@@ -69,8 +82,36 @@ function canvasUI(){
         drawLine(0,800,400,800);
     }
 
+    /***
+    * Defines the blocks that will be used
+    */
+    function defineBlocks(){
+        blocks.push ({
+            id : 'block01',
+            length : 40,
+            color : 'red',
+            onRow : 0,
+            onColumn : 0
+        });
+
+    }
+
+    /***
+    * Draws an specific block from the block array
+    */
+    function drawBlock(blockIndex){
+        var currentBlock = blocks[blockIndex];
+        context.strokeStyle = currentBlock.color;
+        drawSquare(0,0,currentBlock.length);
+    }
+
     function drawScreen() {
         drawGrid();
+        defineBlocks();
+        if(blocks[0].onRow<21){
+            blocks[0].onRow++;
+        }
+        drawBlock(0);
     }
 
 
