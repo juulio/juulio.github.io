96175939538f9c3b4ada031d248aa893e383770d
Julio Del Valle <julio.delvalle@possible.com>
2015-11-11 23:05:26 -0600

    Adding particles.js.min files and sample code

---
+++ experiments/particles.js/app.js
@@ -0,0 +1,6 @@
+console.log('hola');
+
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
