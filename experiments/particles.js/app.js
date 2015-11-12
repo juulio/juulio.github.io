8757a1df2d6df99aae60ad609ab42ae37cca1c5c
Julio Del Valle <julio.delvalle@possible.com>
2015-11-11 23:06:37 -0600

    Removing console.log

--- experiments/particles.js/app.js
+++ experiments/particles.js/app.js
@@ -1,5 +1,3 @@
-console.log('hola');
-
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
