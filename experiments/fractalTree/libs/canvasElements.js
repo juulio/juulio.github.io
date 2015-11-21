add3563293d67e90a2df76289c310cd64b4e121d
Julio Del Valle <julio.delvalle@possible.com>
2015-11-20 23:17:13 -0600

    Adding limitLoop function to control Frame Rate

--- experiments/fractalTree/libs/canvasElements.js
+++ experiments/fractalTree/libs/canvasElements.js
@@ -49,3 +49,34 @@ function sin (angle) {
 function deg_to_rad(angle){
     return angle*(Math.PI/180.0);
 }
+
+/**************************************
+* Function that limits the frame rate */
+var limitLoop = function (fn, fps) {
+
+    // Use var then = Date.now(); if you
+    // don't care about targetting < IE9
+    var then = new Date().getTime();
+
+    // custom fps, otherwise fallback to 60
+    fps = fps || 60;
+    var interval = 1000 / fps;
+
+    return (function loop(time){
+        requestAnimationFrame(loop);
+
+        // again, Date.now() if it's available
+        var now = new Date().getTime();
+        var delta = now - then;
+
+        if (delta > interval) {
+            // Update time
+            // now - (delta % interval) is an improvement over just
+            // using then = now, which can end up lowering overall fps
+            then = now - (delta % interval);
+
+            // call the fn
+            fn();
+        }
+    }(0));
+};
