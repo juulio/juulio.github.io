ee18a6318175c345fbad740f99eb168d7810b56f
Julio Del Valle <julio.delvalle@possible.com>
2015-11-26 21:38:57 -0600

    Testing animated leaves

--- experiments/fractalTree/treeWithAnimatedLeaves.js
+++ experiments/fractalTree/treeWithAnimatedLeaves.js
@@ -42,58 +42,63 @@ var leaves = [];
 
 var Leaf = {
     posX : 0,
-    posY : 0,
+    posY : 0
 
-    run : function(){
-        this.update();
-        this.draw();
-    },
-    update : function(){
-        this.posX += canvasElements.getRandomInt(-5,5);
-        this.posY += canvasElements.getRandomInt(-5,5);
-    },
-    draw : function(){
-        canvasElements.drawDot(this.posX, this.posY, 5, 2, 2, context);
-    }
+    // run : function(){
+    //     this.update();
+    //     this.draw();
+    // },
+    // update : function(){
+    //     this.posX += canvasElements.getRandomInt(-5,5);
+    //     this.posY += canvasElements.getRandomInt(-5,5);
+    // },
+    // draw : function(){
+    //     canvasElements.drawDot(this.posX, this.posY, 5, 2, 2, context);
+    // }
 };
 
 var growTree = function(x1, y1, angle, treeDepth, lineLength){
     var x2 = x1 + (canvasElements.cos(angle) * treeDepth * lineLength),
-        y2 = y1 + (canvasElements.sin(angle) * treeDepth * lineLength);
+        y2 = y1 + (canvasElements.sin(angle) * treeDepth * lineLength),
+        Leaf = {
+            posX : 0,
+            posY : 0
+        };
 
         context.strokeStyle = 'rgb(' + canvasElements.getRandomInt(0,255) +',' + canvasElements.getRandomInt(0,255) +'    ,34)';
 
     if(treeDepth > 0) {
         treeDepth--;
-
         canvasElements.drawLine(x1, y1, x2, y2, context);
 
         growTree(x2, y2, angle - canvasElements.getRandomInt(20,26), treeDepth, lineLength);
         growTree(x2, y2, angle + canvasElements.getRandomInt(30,58), treeDepth, lineLength);
     }
     else {
-        console.log('x2: ' + x2 + '     ' + 'y2: ' + y2);
-        canvasElements.drawDot(x2,y2, 5, '#000', 2, context);
-        // Leaf.posX = x2;
-        // Leaf.posY = y2;
-        // leaves.push(Leaf);
+        Leaf.posX = x2;
+        Leaf.posY = y2;
+        leaves.push(Leaf);
     }
 };
 
 var animateLeaves = function(){
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    var _posX,
+        _posY;
     for(var i =0;i<leaves.length;i++){
-        leaves[i].run();
-        console.log('posX :' + leaves[i].posX);
-        console.log('posY :' + leaves[i].posY);
-        console.log('-----------------------');
+        _posX = leaves[i].posX + canvasElements.getRandomInt(-5,5);
+        _posY = leaves[i].posY + canvasElements.getRandomInt(-5,5);
+
+        canvasElements.drawDot(_posX, _posY, 5, '#000', 2, context);
     }
+
+    requestAnimationFrame(animateLeaves);
 }
 /***********************************************
 * Draws all the elements on the screen */
 var drawScreen = function(){
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    growTree(canvas.width*0.5, canvas.height, -90, 1, 20);
-    // animateLeaves();
+    growTree(canvas.width*0.5, canvas.height, -90, 3, 20);
+    animateLeaves();
 };
 
 drawScreen();
