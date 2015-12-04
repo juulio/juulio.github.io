56272a24753be16fbc57ab659274e4a599dd0cfd
Julio Del Valle <julio.delvalle@possible.com>
2015-12-04 11:14:17 -0600

    Fixing Particles Title

--- js/canvasExperiments/particles-title.js
+++ js/canvasExperiments/particles-title.js
@@ -150,3 +150,22 @@ window.Title = (function(win, $, TweenMax, THREEx) {
     };
 
 })(window, this.jQuery, this.TweenMax, this.THREEx);
+
+
+(function($, win, title) {
+
+    function render() {
+
+        title.render();
+
+        //-- http://css-tricks.com/using-requestanimationframe/
+        //-- Runs at 60 frames per second
+        requestAnimationFrame(render);
+    }
+
+    $(win).ready(function() {
+        title.init($('.title'));
+        render();
+    });
+
+})(this.jQuery, window, window.Title);
