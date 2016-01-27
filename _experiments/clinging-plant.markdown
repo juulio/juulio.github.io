0dc036aeba668cdc355403302cb1951b055bdf6a
Julio Del Valle <julio.delvalle@possible.com>
2016-01-26 21:12:59 -0600

    Fixing clinging plant experiment errors

--- _experiments/clinging-plant.markdown
+++ _experiments/clinging-plant.markdown
@@ -4,6 +4,5 @@ title: Clinging Plant
 description: This code is a simulation of a Clinging Plant. (will be added to some of the Fractal trees)
 permalink: /clinging-plant/
 ---
-
 <script src="/js/libs/canvasElements.js"></script>
 <script type="text/javascript" src="/js/canvasExperiments/clinging-plant.js"></script>
--- js/canvasExperiments/clinging-plant.js
+++ js/canvasExperiments/clinging-plant.js
@@ -1,29 +1,48 @@
-/********************************************************
- Initial code to create and set up the Canvas Element. */
-canvasElements.createCanvasElement('post-content');
-var canvas = canvasElements.canvas;
+/***
+ * Julio Del Valle - Computer Graphics - Costa Rica - 2016
+ * juulio.github.io
+ */
+var clingingPlant = clingingPlant || {};
 
-/***************************************************
- Recursive function that draws The Clinging Plant */
-function recursiveDrawClingingPlant(x, y, plantColumns, plantRows, spaceBetweenRows, spaceBetweenColumns){
-    var dotHorizontalPos = 0,
-        dotVerticalPos = spaceBetweenRows,
-        leftMostPoint = x-(((plantColumns-1)*spaceBetweenColumns)/2);
+(function (context) {
 
-    dotHorizontalPos = leftMostPoint;
-    y+=spaceBetweenRows;
+    /***
+     * Init all required functions
+     */
+    function init () {
 
-    for(var j=0; j<plantColumns; j++){
-        canvasElements.drawLeaf(dotHorizontalPos, y, canvasElements.getRandomInt(50, 130), 3, 0.6);
-        dotHorizontalPos += spaceBetweenColumns;
+        /***************************************************
+         Recursive function that draws The Clinging Plant */
+        function recursiveDrawClingingPlant(x, y, plantColumns, plantRows, spaceBetweenRows, spaceBetweenColumns){
+            var dotHorizontalPos = 0,
+                dotVerticalPos = spaceBetweenRows,
+                leftMostPoint = x-(((plantColumns-1)*spaceBetweenColumns)/2);
+
+            dotHorizontalPos = leftMostPoint;
+            y+=spaceBetweenRows;
+
+            for(var j=0; j<plantColumns; j++){
+                canvasElements.drawLeaf(dotHorizontalPos, y, canvasElements.getRandomInt(50, 130), 3, 0.6);
+                dotHorizontalPos += spaceBetweenColumns;
+            }
+
+            spaceBetweenColumns+=0.8;
+            plantRows--;
+
+            if(plantRows>0) {
+                recursiveDrawClingingPlant(x, y, plantColumns, plantRows, spaceBetweenRows, spaceBetweenColumns, false);
+            }
+        }
+
+        /********************************************************
+         Initial code to create and set up the Canvas Element. */
+        canvasElements.createCanvasElement('post-content');
+        var canvas = canvasElements.canvas;
+
+        recursiveDrawClingingPlant(canvas.width/2, 20, 9, 18, 22, 6);
+
     }
 
-    spaceBetweenColumns+=0.8;
-    plantRows--;
+    init();
 
-    if(plantRows>0) {
-        recursiveDrawClingingPlant(x, y, plantColumns, plantRows, spaceBetweenRows, spaceBetweenColumns, false);
-    }
-}
-
-recursiveDrawClingingPlant(canvas.width/2, 20, 9, 18, 22, 6);
+}(clingingPlant));
