035842d3cc97bac4ef2355999fe7672bcd1c76af
Julio Del Valle <julio.delvalle@possible.com>
2016-06-02 19:09:12 -0600

    Adding THREE.js to thneed file

--- three/thneed/thneedWithThree.js
+++ three/thneed/thneedWithThree.js
@@ -9,8 +9,8 @@ var canvas = document.createElement("canvas"),
 document.body.appendChild(canvas);
 document.body.style.margin = 0;
 
-canvas.width = 400;
-canvas.height = 400;
+canvas.width = 150;
+canvas.height = 180;
 canvas.style.display = 'block';
 canvas.style.margin = '0 auto';
 
@@ -138,7 +138,7 @@ var particleSystem = function(systemCenterPoint){
 /******************************************************************
   Global Variables */
 var systemOuterRadius = 60,
-    systemCenter = new point2D(200, 200),
+    systemCenter = new point2D(70, 100),
     ps = new particleSystem(systemCenter);
 
 /******************************************************************
@@ -164,3 +164,126 @@ function drawScreen(){
 /******************************************************************
   Init the render loop*/
   update();
+
+
+
+
+
+
+/*************************************************************************************************************
+/*************************************************************************************************************
+/*************************************************************************************************************
+/*************************************************************************************************************
+/*************************************************************************************************************
+/*************************************************************************************************************
+ Begin code using THREE.js */
+
+//---------------------------------------------------------
+// Animated: this function is executed each animation frame
+function animate(){
+  drawScene();
+  renderer.render(scene, camera);
+  requestAnimationFrame(animate);
+ }
+
+//---------------------------------------------------------
+// Create renderer object for THREE.js
+var renderer = new THREE.WebGLRenderer();
+renderer.setClearColor( 0x000000 );
+// renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(600, 700);
+document.body.appendChild(renderer.domElement);
+
+//---------------------------------------------------------
+// Create scene object
+var scene = new THREE.Scene();
+
+
+
+//---------------------------------------------------------
+function drawScene(){
+
+}
+
+//---------------------------------------------------------
+// Create camera object
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+
+camera.position.z = 10;
+
+//---------------------------------------------------------
+// Create lighting object
+var ambientLight = new THREE.AmbientLight(0x000044);
+var directionalLight = new THREE.DirectionalLight(0xffffff);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(ambientLight);
+scene.add(directionalLight);
+
+
+//---------------------------------------------------------
+// start animation
+animate();
+
+
+
+
+
+
+
+//
+// // revolutions per second
+// var angularSpeed = 0.1,
+// 	lastTime = 0;
+//
+// // Global Variables
+// var scl = 20;
+// var w = 500;
+// var h = 500;
+// var cols = w / scl;
+// var rows = h /scl;
+// var flying = 0;
+// var terrain = [cols];
+//
+// var cols = 6;
+// var rows = 5;
+//
+//  // terrain = [cols][rows];
+
+//
+
+//
+// //-------------------------------------------------
+// var cols = 4;
+// var rows = 2;
+//
+// function drawTriangleStrip(){
+// 	var x, y, face;
+// 	var z = 0;
+//
+// 	var triangleStripGeometry = new THREE.Geometry();
+//
+// 	for(var y=rows; y > 0; y-- ) {
+// 		for(var x=0; x < cols; x++ ) {
+// 				triangleStripGeometry.vertices.push(new THREE.Vector3(x, y, z));
+// 				triangleStripGeometry.vertices.push( new THREE.Vector3(x, y-1, z));
+// 	  }
+// 	}
+//
+// 	var holes = [];
+// 	var triangles = THREE.Shape.Utils.triangulateShape( triangleStripGeometry.vertices, holes);
+//
+// 	for( var i = 0; i < triangles.length; i++ ){
+//     triangleStripGeometry.faces.push( new THREE.Face3( triangles[i][0], triangles[i][1], triangles[i][2] ));
+// 	}
+//
+//
+// 	var triangleStripMaterial = new THREE.MeshBasicMaterial({
+// 		color:0xFFFFFF,
+// 		wireframe:true
+// 	});
+//
+// 	var triangleStripMesh = new THREE.Mesh(triangleStripGeometry, triangleStripMaterial);
+// 	triangleStripMesh.drawMode = THREE.TriangleStripDrawMode;
+// 	triangleStripMesh.position.set(-4.0, 0.0, 3.0);
+// 	scene.add(triangleStripMesh);
+// }
