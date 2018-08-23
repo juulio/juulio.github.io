f6cbd6fd0eff057e5cdb447d2e4a72ed3035ce5e
Julio Del Valle <juulio@gmail.com>
2018-08-23 13:16:58 -0600

    Tweening the trunk properly

--- js/main.js
+++ js/main.js
@@ -27,12 +27,12 @@ const controls = new OrbitControls( camera );
 
 let stats;
 // let trunkMesh, trunkGeometry, dynaimcTrunkHeight;
-// let branchGeometry, branchMesh01, branchMesh02, branchMesh03, branchMesh04, branchLength;
 
 
 
 initScene();
 animate();
+
 
 let woodMaterial = new THREE.MeshBasicMaterial( { color: 0x8B4513 } ),
 	redMaterial = new THREE.MeshBasicMaterial( { color: 0xFF4513, wireframe: true } ),
@@ -40,29 +40,28 @@ let woodMaterial = new THREE.MeshBasicMaterial( { color: 0x8B4513 } ),
 
 let trunkOrigin = new THREE.Vector3(0, 0, 0),
 	trunkRadius = 0.1,
-	trunkHeight = 1,
-	fractalRatio = 0.8;
+	trunkHeight = 2,
+	fractalRatio = 0.90,
+	angleX = Math.PI/7,
+	angleZ = Math.PI/5;
 
-renderTrunk(trunkOrigin, trunkRadius, trunkHeight, woodMaterial);
-
-// 1st level
-let branchOrigin = new THREE.Vector3( trunkOrigin.x, trunkOrigin.y, trunkOrigin.z ),
-	branchMesh,
-	branchParentMesh,
-	angleX = Math.PI/4,
-	angleZ = Math.PI/4,
-	branchRadius = trunkRadius * fractalRatio,
-	branchHeight = trunkHeight * fractalRatio;
-
-scene.add(renderBranch(branchOrigin, branchRadius, branchHeight, angleX, angleZ, trunkHeight, woodMaterial, redMaterial, fractalRatio));
-scene.add(renderBranch(branchOrigin, branchRadius, branchHeight, -angleX, angleZ, trunkHeight, woodMaterial, redMaterial, fractalRatio));
-scene.add(renderBranch(branchOrigin, branchRadius, branchHeight, angleX, -angleZ, trunkHeight, woodMaterial, redMaterial, fractalRatio));
-scene.add(renderBranch(branchOrigin, branchRadius, branchHeight, -angleX, -angleZ, trunkHeight, woodMaterial, redMaterial, fractalRatio));
+renderTree(trunkOrigin, trunkRadius, trunkHeight, fractalRatio, angleX, angleZ);
 
 
-// 2nd level
-// branchOrigin = 
-// renderBranch(branchOrigin, branchRadius, branchHeight, angleX, angleZ, trunkHeight, woodMaterial, redMaterial);
+/**
+  * Render Tree
+  */
+function renderTree(origin, radius, height, fractalRatio, angleX, angleZ){
+	let branchHeight = height * fractalRatio;
+	let branchRadius = radius * fractalRatio;
+
+	renderTrunk(origin, radius, height, woodMaterial);
+
+	scene.add(renderBranch(origin, branchRadius, branchHeight, angleX, angleZ, height, woodMaterial, redMaterial, fractalRatio));
+	scene.add(renderBranch(origin, branchRadius, branchHeight, -angleX, angleZ, height, woodMaterial, redMaterial, fractalRatio));
+	scene.add(renderBranch(origin, branchRadius, branchHeight, angleX, -angleZ, height, woodMaterial, redMaterial, fractalRatio));
+	scene.add(renderBranch(origin, branchRadius, branchHeight, -angleX, -angleZ, height, woodMaterial, redMaterial, fractalRatio));
+}
 
 
 /**
@@ -72,12 +71,26 @@ scene.add(renderBranch(branchOrigin, branchRadius, branchHeight, -angleX, -angle
   * @param {Number} height
   * @param {Number} angle
   * CylinderBufferGeometry(radiusTop : Float, radiusBottom : Float, height : Float)
+  * mesh.scale.x = 2 or mesh.scale.set( 2, 1, 1 )
   */
 function renderTrunk(origin, radius, height, material){
-	let trunkGeometry = new THREE.CylinderBufferGeometry( radius, radius, height),
+	let trunkGeometry = new THREE.CylinderBufferGeometry( radius, radius, 0),
 		mesh = new THREE.Mesh( trunkGeometry, material );
-	
-	mesh.position.set(origin.x, origin.y + height/2, origin.z);
+		mesh.scale.y = 0;
+
+	let tween = new TWEEN.Tween( mesh.scale )
+    .to( {
+    		y: height	
+    	}, 2500
+    )
+    .onUpdate(
+    	function(){
+			mesh.position.set(origin.x, origin.y + mesh.scale.y/2, origin.z);
+			// mesh.position.set(origin.x, origin.y + height/2, origin.z);
+    	}
+    );
+    tween.start();
+	// mesh.position.set(origin.x, origin.y + height/2, origin.z);
 	scene.add(mesh);
 }
 
@@ -93,20 +106,16 @@ function renderTrunk(origin, radius, height, material){
 function renderBranch(origin, radius, height, angleX, angleZ, branchParentPositionY, material, parentMaterial, scalingFactor){
 	let branchDiameter = radius * 2,
 		branchGeometry = new THREE.CylinderBufferGeometry( radius, radius, height ),
-		boxGeometry = new THREE.BoxBufferGeometry( branchDiameter, branchHeight, branchDiameter ),
+		boxGeometry = new THREE.BoxBufferGeometry( branchDiameter, height, branchDiameter ),
 		branchParentMesh = new THREE.Mesh( boxGeometry, parentMaterial ),
 		branchMesh = new THREE.Mesh ( branchGeometry, material );
 
 	branchMesh.position.set(origin.x, origin.y + height/2, origin.z);
-	// branchMesh.position.set(-(Math.cos(angleZ) * branchHeight / 2 ), origin.y + height/2, origin.z);
 	branchParentMesh.position.set(origin.x, branchParentPositionY, origin.z);
-	// branchParentMesh.position.set(-(Math.cos(angleZ) * branchHeight / 2 ), branchParentPositionY, origin.z);
 	branchParentMesh.rotation.set(angleX, 0, angleZ);
 
 	branchParentMesh.add(branchMesh);
-	// scene.add(branchParentMesh);
 
-	// origin = new THREE.Vector3(origin.x, origin.y, origin.z);
 	branchParentPositionY *= scalingFactor;
 
 	radius *= scalingFactor;
@@ -115,7 +124,7 @@ function renderBranch(origin, radius, height, angleX, angleZ, branchParentPositi
 	angleZ *= scalingFactor;
 
 	scalingFactor *= scalingFactor;
-	material = greenMaterial;
+	// material = greenMaterial;
 
 	if(scalingFactor > 0.6) {
 		branchParentMesh.add(renderBranch(origin, radius, height, angleX, angleZ, branchParentPositionY, material, parentMaterial, scalingFactor));
@@ -161,7 +170,7 @@ function initScene(){
 	scene.add( getAxesHelper(50) );
 	// scene.add( getAmbientLight(0x404040) );
 
-	camera.position.set(0, 2.2, 1.8);
+	camera.position.set(0, -1, 4);
 	camera.lookAt(0, 0, 0);
 	document.body.appendChild( renderer.domElement );
 
@@ -211,122 +220,84 @@ function animate(){
 
 
 
+/*
+function renderBranch(origin, radius, height, angle, scalingFactor, isTrunk){
+	let branchGeometry = new THREE.CylinderGeometry( radius, radius, height, 10, 10);
+	let mesh = new THREE.Mesh( branchGeometry, woodMaterial );
+	mesh.geometry.computeBoundingBox();
+	mesh.scale.y = 0.01;
 
-
-
-
-// let mesh = new THREE.Mesh( branchGeometry, woodMaterial );
-// mesh.rotation.set(0, 0, angle);
-// mesh.position.set(-(Math.cos(angle) * branchHeight / 2 ), trunkHeight+trunkHeight/(2*Math.PI/angle), 0);
-// // mesh.position.set(-(Math.cos(angle) * branchHeight / 2 ), trunkHeight+branchHeight/2/2, 0);
-// // scene.add(mesh);
-
-// mesh = new THREE.Mesh( branchGeometry, woodMaterial );
-// mesh.rotation.set(0, 0, -angle);
-// mesh.position.set((Math.cos(angle) * branchHeight / 2 ), trunkHeight, 0);
-// scene.add(mes3h);
-
-
-// renderBranch(trunkOrigin, trunkRadius, trunkHeight, angle, scalingFactor, true);
-
-// renderBranch(new THREE.Vector3(0, 0, 0), trunkRadius, trunkHeight, angle, scalingFactor, true);
-// renderBranch(new THREE.Vector3(0, trunkHeight * fractalRatio, 0), 2, trunkHeight*0.8, Math.PI/4, fractalRatio);
-// renderBranch(new THREE.Vector3(5, 0, 0), 2, 0);
-// renderBranch(new THREE.Vector3(5, 0, 0), 2, 0, Math.PI / 4);
-// renderBranch(2, 0, Math.PI / 4);
-// renderBranch(2, 0, Math.PI / 4);
-
-/**
- * draw branch
- * @param {THREE.Vector3( x, y, z);} origin
- * @param {Number} radius
- * @param {Number} height
- * @param {Number} angle
- * CylinderGeometry(radiusTop : Float, radiusBottom : Float, height : Float, radialSegments : Integer, heightSegments : Integer);
- * mesh.rotation.set(0, 90, 180);
- */
-// function renderBranch(origin, radius, height, angle, scalingFactor, isTrunk){
-// 	let branchGeometry = new THREE.CylinderGeometry( radius, radius, height, 10, 10);
-// 	// branchGeometry.translate(origin.x, origin.y, origin.z);
-// 	let mesh = new THREE.Mesh( branchGeometry, woodMaterial );
-// 	mesh.geometry.computeBoundingBox();
-// 	mesh.scale.y = 0.01;
-
-// 	if(!isTrunk) {
-// 		mesh.rotation.set(0, 0, angle);
-// 	}
+	if(!isTrunk) {
+		mesh.rotation.set(0, 0, angle);
+	}
 	
-// 	// mesh.rotation.set(0, 0, angle);
+	scene.add( mesh );
 
-// 	scene.add( mesh );
+	let tween = new TWEEN.Tween( mesh.scale )
+    .to( {
+    		y: scalingFactor
+    	}, 1600
+    )
+    .onUpdate(
+    	function(){
+			let posX, posY;
 
-// 	let tween = new TWEEN.Tween( mesh.scale )
-//     .to( {
-//     		y: scalingFactor
-//     	}, 1600
-//     )
-//     .onUpdate(
-//     	function(){
-// 			let posX, posY;
+			if (!isTrunk) {
+				posX = height/2  * Math.cos(angle);
+			}
+			else {
+				posX = origin.x;
+			}
 
-// 			if (!isTrunk) {
-// 				posX = height/2  * Math.cos(angle);
-// 			}
-// 			else {
-// 				posX = origin.x;
-// 			}
+			posY = origin.y + mesh.scale.y*height/2
+			mesh.position.set(origin.x, origin.y + mesh.scale.y*height/2, origin.z);
+    	}
+    )
+    .onComplete(
+    	function(){
+			let x = height * Math.cos(angle) / 2;
+			console.log(x);
 
-// 			posY = origin.y + mesh.scale.y*height/2
-
-
-// 			// mesh.position.set(posX, posY, origin.z);
-// 			mesh.position.set(origin.x, origin.y + mesh.scale.y*height/2, origin.z);
-//     	}
-//     )
-//     .onComplete(
-//     	function(){
-// 			let x = height * Math.cos(angle) / 2;
-// 			console.log(x);
-
-// 			// mesh.position.set(x, origin.y + mesh.scale.y*height/2, origin.z);
+			// mesh.position.set(x, origin.y + mesh.scale.y*height/2, origin.z);
 
 
-//     		if(scalingFactor > 1){
-// 				let	max = mesh.geometry.boundingBox.max;
+    		if(scalingFactor > 1){
+				let	max = mesh.geometry.boundingBox.max;
 
-//     			let newPos = new THREE.Vector3( origin.x, scalingFactor*2*max.y+origin.y, origin.z );
+    			let newPos = new THREE.Vector3( origin.x, scalingFactor*2*max.y+origin.y, origin.z );
     			
-//     			scalingFactor *= 0.8;
-//     			height *= scalingFactor;
-//     			angle *= scalingFactor;
+    			scalingFactor *= 0.8;
+    			height *= scalingFactor;
+    			angle *= scalingFactor;
 				
-// 				renderBranch(newPos, radius, height, angle, scalingFactor, false);
-// 				// renderBranch(newPos, radius, height, -angle, scalingFactor, false);
-//     		}
-// 			// mesh.geometry.computeBoundingBox();
-// 			// let	max = mesh.geometry.boundingBox.max,
-// 			// 	box = new THREE.BoxHelper( mesh, 0xf00f00 );
-// 			// scene.add( box );
+				renderBranch(newPos, radius, height, angle, scalingFactor, false);
+				// renderBranch(newPos, radius, height, -angle, scalingFactor, false);
+    		}
+			// mesh.geometry.computeBoundingBox();
+			// let	max = mesh.geometry.boundingBox.max,
+			// 	box = new THREE.BoxHelper( mesh, 0xf00f00 );
+			// scene.add( box );
 
-// 			// let boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-// 			// let maxBoxMesh = new THREE.Mesh( boxGeometry, maxBoxMaterial);
+			// let boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+			// let maxBoxMesh = new THREE.Mesh( boxGeometry, maxBoxMaterial);
 
-// 			// maxBoxMesh.position.set(origin.x, scalingFactor*2*max.y+origin.y, origin.z);
-// 			// // maxBoxMesh.position.set(origin.x  , scalingFactor  * max.y, origin.z );
-// 			// // maxBoxMesh.position.set(origin.x  , scalingFactor  * max.y, origin.z );
-// 			// scene.add(maxBoxMesh);
+			// maxBoxMesh.position.set(origin.x, scalingFactor*2*max.y+origin.y, origin.z);
+			// // maxBoxMesh.position.set(origin.x  , scalingFactor  * max.y, origin.z );
+			// // maxBoxMesh.position.set(origin.x  , scalingFactor  * max.y, origin.z );
+			// scene.add(maxBoxMesh);
 
-// 			// console.log('max.y ' + max.y);
-// 			// console.log('scalingFactor ' + scalingFactor);
-// 			// console.log('origin.y ' + origin.y);
-// 			// console.log('min = (' + min.x + ', ' + min.y + ', ' + min.z + ')');
-// 			// console.log('maxBoxMesh.position = Vec3(' + maxBoxMesh.position.x + ', ' + maxBoxMesh.position.y + ', ' + maxBoxMesh.position.z + ')');
+			// console.log('max.y ' + max.y);
+			// console.log('scalingFactor ' + scalingFactor);
+			// console.log('origin.y ' + origin.y);
+			// console.log('min = (' + min.x + ', ' + min.y + ', ' + min.z + ')');
+			// console.log('maxBoxMesh.position = Vec3(' + maxBoxMesh.position.x + ', ' + maxBoxMesh.position.y + ', ' + maxBoxMesh.position.z + ')');
 
-//     		// console.log(origin.y + scalingFactor*height);
-//     	}
-//     );
-//     tween.start();
-// }
+    		// console.log(origin.y + scalingFactor*height);
+    	}
+    );
+    tween.start();
+}
+*/
 
 // let branchParams = {
 // 	branchLength : 0,
