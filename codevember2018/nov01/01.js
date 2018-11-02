59c5ed83408867727ccb473cfeffb296b228b45e
Julio Del Valle <juulio@gmail.com>
2018-11-02 10:53:42 -0600

    Updating code

--- codevember2018/nov01/01.js
+++ codevember2018/nov01/01.js
@@ -4,20 +4,17 @@ function setup(){
 
 function draw(){
   background(255);
-
   normalMaterial();
 
   push();
   rotateX(frameCount * 0.01);
   torus(70, 20);
   pop();
-    
+
   push();
   translate(140, 0, 0);
   rotateX(frameCount * -0.01);
 
   torus(70, 20);
   pop();
-
-
 }
