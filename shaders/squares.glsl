7d8a524dfdad251bd0703783cd2525538e1f3930
Julio Del Valle <juulio@gmail.com>
2020-03-30 22:37:47 -0600

    Adding shaders files

--- shaders/squares.glsl
+++ shaders/squares.glsl
@@ -9,9 +9,9 @@ precision mediump float;
 		vec2 st = gl_FragCoord.xy/u_resolution.xy;
 
 		float y = step(0.5, st.x);
-		float x = step(0.5, st.y);
+		float x = step(0.5, st.x);
 
-		vec3 color = vec3(x, y, y);
+		vec3 color = vec3(y, y, y);
 
 		gl_FragColor = vec4(color,1.0);
 	}
