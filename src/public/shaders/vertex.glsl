ea08d5f19b3705dc688bbdf80e08ad3b7accdd59
Julio Del Valle <juulio@gmail.com>
2021-03-17 23:57:31 -0600

    Updating vertex shader

--- src/public/shaders/vertex.glsl
+++ src/public/shaders/vertex.glsl
@@ -1,7 +1,6 @@
-uniform float delta;
-uniform vec2 uvScale;
+varying vec2 vUv;
 
 void main() {
-	vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
-	gl_Position = projectionMatrix * modelViewPosition;
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
 }
