7d61d6c36b032be4fcce2fba578791604ab5c8c5
Julio Del Valle <juulio@gmail.com>
2021-04-05 22:08:55 -0600

    adding scracth project folder

---
+++ docs/scratch/js/scratch-card.js
@@ -0,0 +1,120 @@
+// https://www.codehim.com/others/create-scratch-card-using-javascript-html5-canvas/
+// otro ejemplo https://masth0.github.io/ScratchCard/
+// Ejemplo con pixiJS https://pixijs.io/examples/#/demos-advanced/scratchcard.js
+
+(function(){
+
+  'use strict';
+
+  var canvas = document.getElementById('scratch'),
+      context = canvas.getContext('2d');
+
+  // default value
+  context.globalCompositeOperation = 'source-over';
+
+  //----------------------------------------------------------------------------
+
+  var x, y, radius;
+
+  x = y = radius = 150 / 2;
+
+  // fill circle
+  context.beginPath();
+  context.fillStyle = '#515151';
+  context.rect(0, 0, 500, 300);
+  context.fill();
+
+  //----------------------------------------------------------------------------
+
+  var isDrag = false;
+
+  function clearArc(x, y) {
+    context.globalCompositeOperation = 'destination-out';
+    context.beginPath();
+    context.arc(x, y, 10, 0, Math.PI * 2, false);
+    context.fill();
+  }
+
+  canvas.addEventListener('mousedown', function(event) {
+    isDrag = true;
+
+    clearArc(event.offsetX, event.offsetY);
+    //judgeVisible();
+  }, false);
+
+  canvas.addEventListener('mousemove', function(event) {
+    if (!isDrag) {
+      return;
+    }
+
+    clearArc(event.offsetX, event.offsetY);
+    //judgeVisible();
+  }, false);
+
+  canvas.addEventListener('mouseup', function(event) {
+    isDrag = false;
+  }, false);
+
+  canvas.addEventListener('mouseleave', function(event) {
+    isDrag = false;
+  }, false);
+
+  //----------------------------------------------------------------------------
+
+  canvas.addEventListener('touchstart', function(event) {
+    if (event.targetTouches.length !== 1) {
+      return;
+    }
+
+    event.preventDefault();
+
+    isDrag = true;
+
+    clearArc(event.touches[0].offsetX, event.touches[0].offsetY);
+    //judgeVisible();
+  }, false);
+
+  canvas.addEventListener('touchmove', function(event) {
+    if (!isDrag || event.targetTouches.length !== 1) {
+      return;
+    }
+
+    event.preventDefault();
+
+    clearArc(event.touches[0].offsetX, event.touches[0].offsetY);
+    //judgeVisible();
+  }, false);
+
+  canvas.addEventListener('touchend', function(event) {
+    isDrag = false;
+  }, false);
+
+  // document.getElementById('btn-download-canvas-screenshot').addEventListener('click', function(){
+  //   let image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");  // here is the most important part because if you dont replace you will get a DOM 18 exception.
+  //   window.location.href=image;
+  // });
+  
+function takeshot() {
+  
+}
+  //----------------------------------------------------------------------------
+
+  // function judgeVisible() {
+  //   var imageData = context.getImageData(0, 0, 150, 150),
+  //       pixels = imageData.data,
+  //       result = {},
+  //       i, len;
+
+  //   // count alpha values
+  //   for (i = 3, len = pixels.length; i < len; i += 4) {
+  //     result[pixels[i]] || (result[pixels[i]] = 0);
+  //     result[pixels[i]]++;
+  //   }
+
+  //   document.getElementById('gray-count').innerHTML = result[255];
+  //   document.getElementById('erase-count').innerHTML = result[0];
+  // }
+
+  // document.addEventListener('DOMContentLoaded', judgeVisible, false);
+
+}());
