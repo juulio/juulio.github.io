4a5f7dc3d77e484eb91536d3e7cf4386232b6752
Julio Del Valle <juulio@gmail.com>
2021-05-04 00:00:00 -0600

    Adding scratch project

--- docs/scratch/js/scratch.js
+++ docs/scratch/js/scratch.js
@@ -1,5 +1,10 @@
 let sc, scratchContainerHeight;
-let imagesArray = ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg', './img/06.jpg', './img/07.jpg', './img/08.jpg', './img/09.jpg', './img/10.jpg', './img/11.gif','./img/12.gif', './img/13.jpg','./img/14.jpg','./img/15.jpg'];
+let imagesArray = ['./img/01.jpg', './img/02.jpg', './img/03.jpg', './img/04.jpg', './img/05.jpg', './img/06.jpg', './img/07.gif',
+ './img/08.gif', './img/09.jpg', './img/10.jpg', './img/11.jpg','./img/12.jpg', './img/13.gif','./img/14.gif','./img/15.jpg',
+ './img/16.jpg', './img/17.jpg', './img/18.jpg', './img/19.jpg','./img/20.jpg', './img/21.gif','./img/22.gif','./img/23.jpg',
+ './img/24.jpg', './img/25.jpg', './img/26.jpg', './img/27.jpg','./img/28.jpg', './img/29.gif','./img/30.gif','./img/31.jpg',
+ './img/32.jpg', './img/33.jpg', './img/34.jpg'];
+ 
 const imagesArrayLength = imagesArray.length;
 let isMobileDevice = (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
 const scContainer = document.getElementById('js--sc--container')
@@ -84,4 +89,4 @@ let preloadImages = ( images ) => {
 
 // Run the project
 preloadImages(imagesArray);
-setupScratchCard('./img/portada.jpg', getRandomImagePath());
+setupScratchCard('./img/portada.png', getRandomImagePath());
