d427aef63ed08d80474e819fa204205378190681
Julio Del Valle <juulio@gmail.com>
2021-09-30 18:09:11 -0600

    Updating the whole site as threejs portfolio

---
+++ docs/js/main.js
@@ -0,0 +1,327 @@
+/**
+ * Julio Del Valle
+ * Possible
+ * Costa Rica 2016
+ */
+
+'use strict';
+
+var juliodelvalle = window.juliodelvalle || {};
+
+(function (context) {
+
+    /*
+     * Declare all global variables
+     */
+
+    let scene, loader, camera, controls, renderer,
+        clock, geometry, isMobile, windowHalfX, windowHalfY,
+        shaderMaterials, planeMesh, // TODO create a plane mesh with lava material
+        fontLoader, textMesh, letterPosition,
+        uniforms, manager;
+
+    /*
+     * Inits all functions
+     */
+    function init(font) {
+        // Verifies if app is running on a mobile device
+        isMobile = false;
+
+        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+            isMobile = true;
+        }
+
+        window.addEventListener( 'resize', onWindowResize, false );
+
+        setScene();
+
+        setupShaderMaterials();
+
+        renderTextGeometry(font);
+        
+        onWindowResize();
+        
+        update();
+    }
+
+    /*
+     * Updates all the elements on the screen - requestAnimationFrame
+     */
+    function update() {
+        requestAnimationFrame(update);
+
+        var delta = clock.getDelta();
+        uniforms.u_time.value += delta * 2;
+
+        if(letterPosition < textMesh.children.length) {
+            rotateLetters();
+        }
+
+        controls.update();
+
+        renderer.render(scene, camera);
+    }
+
+    /*
+     * Init Uniforms for shaderMaerial
+     * TODO: create a shaderMaterial array to use several shaders on several materials
+     */
+    function setupShaderMaterials(){
+        shaderMaterials = [];
+
+        uniforms = {
+            u_time: { type: "f", value: 1.0 },
+            u_resolution: { type: "v2", value: new THREE.Vector2() },
+            u_mouse: { type: "v2", value: new THREE.Vector2() }
+        };
+
+        uniforms.u_resolution.value.x = window.innerWidth;
+        uniforms.u_resolution.value.y = window.innerHeight;
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Voronoi",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'voronoiFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Jaguar Texture",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'jaguarFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Red Pulse",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'redPulseFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Black & White Matrix",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'bwMatrixFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Rotated Tiles",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'rotatedTilesFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Noise",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'noiseFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Simplex Grid",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'simplexGridFragmentShader' ).textContent
+            })
+        );
+
+        shaderMaterials.push(
+            new THREE.ShaderMaterial( {
+                name: "Displacement",
+                uniforms: uniforms,
+                vertexShader: document.getElementById( 'vertexShader' ).textContent,
+                fragmentShader: document.getElementById( 'displacementFragmentShader' ).textContent
+            })
+        );
+    }
+
+    /*
+     * Returns true if a given number n is Even
+     */
+    function isEven(n) {
+        return n % 2 == 0;
+    }
+
+    /*
+     * Basic THREE.js scene init
+     */
+    function setScene() {
+        let canvasContainer = document.getElementById('canvasContainer'),
+            // rendererHeight = canvasContainer.clientHeight,
+            rendererWidth = '600px',
+            rendererHeight = '400px';
+            // rendererWidth = canvasContainer.clientWidth;
+            console.log(rendererHeight);
+            console.log(rendererWidth);
+
+            // rendererHeight = 300,
+            // rendererWidth = 700;
+        // rendererWidth = window.innerWidth;
+
+        renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false});
+        renderer.setSize(rendererWidth, rendererHeight);
+        renderer.setPixelRatio(window.devicePixelRatio);
+        renderer.setClearColor(0xFFFFFF);
+
+        scene = new THREE.Scene();
+
+        clock = new THREE.Clock();
+
+        camera = new THREE.PerspectiveCamera(75, rendererWidth / rendererHeight, .1, 50);
+
+        if(isMobile) {
+            camera.position.z = 12;
+        }
+        else {
+            camera.position.z = 6;
+        }
+
+        controls = new THREE.OrbitControls(camera);
+        controls.enablePan = true;
+        controls.enableZoom = true;
+
+        var light = new THREE.AmbientLight( 0x000000 );
+        scene.add( light );
+
+        var lights = [];
+        lights[ 0 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+        lights[ 1 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+        lights[ 2 ] = new THREE.PointLight( 0xffffff, 1, 0 );
+
+        lights[ 0 ].position.set( 0, 200, 0 );
+        lights[ 1 ].position.set( 100, 200, 100 );
+        lights[ 2 ].position.set( - 100, - 200, - 100 );
+
+        scene.add( lights[ 0 ] );
+        scene.add( lights[ 1 ] );
+        scene.add( lights[ 2 ] );
+
+        // var axisHelper = new THREE.AxisHelper( 5 );
+        // scene.add( axisHelper );
+
+        canvasContainer.appendChild( renderer.domElement );
+    }
+
+    /*
+     * Loads the JSON font and call
+     */
+    function renderTextGeometry(font){
+        let theText = "POSSIBLE",
+        letterWidth = 0,
+        letterMesh;
+
+        letterPosition = 0;
+
+        textMesh = new THREE.Group();
+
+        for(let i=0;i<theText.length;i++){
+            geometry = new THREE.TextGeometry( theText[i], {
+                font: font,
+                size: 1.15,
+                height: 0.25,
+                curveSegments: 20
+            });
+
+            geometry.center();
+
+            letterMesh = new THREE.Mesh( geometry, shaderMaterials[i] );
+
+            letterMesh.position.x = i;
+
+            textMesh.add( letterMesh)
+        }
+
+        textMesh.position.x = -4;
+        textMesh.position.y = 2;
+
+        if(isMobile){
+            textMesh.position.y = 3.5;
+        }
+
+        scene.add(textMesh);
+    }
+
+    /*
+     * Rotates each letter on the Y Axis
+     */
+    function rotateLetters(){
+        let nextLetterRotationY,
+        rotationSpeed = 0.22,
+        currentLetterRotationY = textMesh.children[letterPosition].rotation.y;
+
+        // Rotate Current Letter on the Y Axis
+        textMesh.children[letterPosition].rotation.y += rotationSpeed;
+
+        if(textMesh.children[letterPosition].rotation.y >= 6.28) {
+            letterPosition++;
+        }
+
+        if(letterPosition < textMesh.children.length-1) {
+            if(currentLetterRotationY >= 2) {
+                textMesh.children[letterPosition + 1].rotation.y += rotationSpeed;
+            }
+        }
+    }
+
+    /*
+     * Handles onWindowResize event and updates Projection Matrix
+     */
+    function onWindowResize() {
+        windowHalfX = window.innerWidth / 2;
+        windowHalfY = window.innerHeight / 2;
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    /*
+     * Handles onDocumentTouchStart Event
+     */
+    function onDocumentTouchStart( event ) {
+
+        if ( event.touches.length == 1 ) {
+            event.preventDefault();
+            mouseXOnMouseDown = event.touches[ 0 ].pageX - windowHalfX;
+            targetRotationOnMouseDown = targetRotation;
+        }
+    }
+
+    /*
+     * Handles onDocumentTouchMove Event
+     */
+    function onDocumentTouchMove( event ) {
+
+        if ( event.touches.length == 1 ) {
+            event.preventDefault();
+            mouseX = event.touches[ 0 ].pageX - windowHalfX;
+            targetRotation = targetRotationOnMouseDown + ( mouseX - mouseXOnMouseDown ) * 0.05;
+        }
+    }
+
+    /*
+     * Load the JSON font and call init
+     */
+    loader = new THREE.FontLoader();
+    loader.load('./fonts/gotham_black_regular.json', function(font){
+        init(font);
+    });
+
+}(juliodelvalle));
---
+++ docs/projects/amusement-park/js/main.js
@@ -0,0 +1,178 @@
+/*
+ * Julio Del Valle
+ * Costa Rica
+ * 2017
+ */
+
+var generative_graphics = window.generative_graphics || {};
+
+/*
+ * Global logic
+ * @namespace
+ */
+
+(function (generative_graphics){
+
+    'use strict';
+
+    /**
+     * Rotates Minaret's Booth on Y Axis
+     */
+    var rotateMinarets = function(){
+        for(var i=0;i<generative_graphics.minaretsGroup.children.length;i++){
+            var currentMinaret = generative_graphics.minaretsGroup.children[i];
+            currentMinaret.children[3].rotation.y += 0.005;
+        }
+    };
+
+    /**
+     * Rotates Ferris Wheel on Y Axis
+     */
+    var rotateFerrisWheel = function(){
+        generative_graphics.ferrisWheel.rotation.y -= 0.003;
+    };
+
+    /**
+     * Rotates Pyramid on Y Axis
+     */
+    let pyramidLevelIndex = 0;
+
+    var rotatePyramid = function(){
+        if(generative_graphics.pyramid.children[pyramidLevelIndex].rotation.y < Math.PI){
+            generative_graphics.pyramid.children[pyramidLevelIndex].rotation.y += 0.08;
+        }
+         else {
+            generative_graphics.pyramid.children[pyramidLevelIndex].rotation.y = 0;
+            pyramidLevelIndex++;
+
+            if(pyramidLevelIndex >= generative_graphics.pyramidLevels-1){
+                pyramidLevelIndex = 0;
+            }
+        }
+    };
+
+    /** 
+     * Init all functions
+     */
+    var scene, camera, renderer, container, controls;
+    var cube;
+    var updateFcts = [];
+
+    generative_graphics.updateFcts = updateFcts;
+    
+    setScene();
+
+    var scene01 = generative_graphics.initScene01();
+    generative_graphics.scene.add(scene01);
+
+    var scene02 = generative_graphics.initScene02();
+    generative_graphics.scene.add(scene02);
+    
+    renderHelpers();
+
+    animate();
+
+    // generative_graphics.initNav();
+
+    /**
+     * Clears All Elements form the Scene
+     */
+    generative_graphics.clearScene = function(){
+        while(scene.children.length > 0){ 
+            scene.remove(scene.children[0]); 
+        }   
+    };
+
+    /*
+     * Renders 3D Cube
+     */
+    generative_graphics.renderCube = function(){
+        // Create a Cube Mesh with basic material
+        var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
+        var cube = new THREE.Mesh( geometry, material );
+
+        // Add cube to Scene
+        scene.add( cube );
+    };
+
+    /*
+     * Sets basic 3D Scene Elements
+     */
+    function setScene(){
+        scene = new THREE.Scene();
+
+        generative_graphics.scene = scene;
+
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+        camera.position.x = 20;
+        camera.position.y = 50;
+        camera.position.z = 140;
+
+        generative_graphics.camera = camera;
+
+        renderer = new THREE.WebGLRenderer( );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setClearColor( 0xFFFFFF, 1 );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        document.body.appendChild( renderer.domElement );
+
+        controls = new THREE.OrbitControls( camera, renderer.domElement );
+        window.addEventListener( 'resize', onWindowResize, false );
+     }
+
+    /*
+     * Handles window resize events
+     */
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+    }
+
+    /**
+     * Updates objects on each frame
+     */
+    function animate(nowMsec) {
+        requestAnimationFrame( animate );
+
+        rotateMinarets();
+
+        rotateFerrisWheel();
+
+        rotatePyramid();
+        
+        var lastTimeMsec  = lastTimeMsec || nowMsec-1000/60;
+        var deltaMsec = Math.min(200, nowMsec - lastTimeMsec);
+        lastTimeMsec  = nowMsec;
+        
+        // call each update function
+        updateFcts.forEach(function(updateFn){
+            updateFn(deltaMsec/1000, nowMsec/1000);
+        });
+        
+        renderer.render( scene, camera );
+    }
+
+    /**
+     * Remove an element from the Scene
+     */
+    function removeEntity(object) {
+        var selectedObject = scene.getObjectByName(object.name);
+        scene.remove( selectedObject );
+        animate();
+    }
+
+    /**
+     * Render XYZ Axis Helpers
+     */
+    function renderHelpers(){
+        var gridXZ = new THREE.GridHelper(290, 20);
+        scene.add(gridXZ);
+
+        var axisHelper = new THREE.AxisHelper( 25 );
+        scene.add( axisHelper );
+    }
+
+}(generative_graphics));
