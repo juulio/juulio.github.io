ba25f5b8457bbc3bc67b78f2687715828e18d694
Julio Del Valle <juulio@gmail.com>
2021-10-06 23:03:59 -0600

    Removing wrong legacy multi material error

--- docs/projects/mosque/js/skybox.js
+++ docs/projects/mosque/js/skybox.js
@@ -5,7 +5,7 @@
  */
 
 /** @namespace Namespace for MOSQUE classes and functions. */
-var MOSQUE = MOSQUE || {};
+MOSQUE = MOSQUE || {};
 
 /**
  * Renders a Skybox
@@ -14,8 +14,8 @@ var MOSQUE = MOSQUE || {};
  * @namespace MOSQUE
  */
 MOSQUE.Skybox = function(boxSize){
-    var urls = [ "./assets/skybox/mars_back.png", "./assets/skybox/mars_front.png", "./assets/skybox/mars_top.png", "./assets/skybox/mars_bottom.png", "./assets/skybox/mars_right.png", "./assets/skybox/mars_left.png"];
-    var cubeMaterials = [
+    let urls = [ "./assets/skybox/mars_back.png", "./assets/skybox/mars_front.png", "./assets/skybox/mars_top.png", "./assets/skybox/mars_bottom.png", "./assets/skybox/mars_right.png", "./assets/skybox/mars_left.png"];
+    let cubeMaterials = [
         // Do not modify the images order
         new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load(urls[0]), side: THREE.DoubleSide } ),
         new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load(urls[1]), side: THREE.DoubleSide } ),
@@ -25,12 +25,9 @@ MOSQUE.Skybox = function(boxSize){
         new THREE.MeshBasicMaterial( {map: new THREE.TextureLoader().load(urls[5]), side: THREE.DoubleSide } ),
     ];
 
-    // Create a MeshFaceMaterial, which allows the cube to have different materials on each face
-    var cubeMaterial = new THREE.MultiMaterial( cubeMaterials );
-
-    var skybox = new THREE.Mesh(
+    let skybox = new THREE.Mesh(
         new THREE.BoxGeometry( boxSize, boxSize, boxSize),
-        cubeMaterial
+        cubeMaterials
     );
 
     return skybox;
