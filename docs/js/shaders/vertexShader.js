a0cc541565783adf096eec73c3ee91349c84afa2
Julio Del Valle <juulio@gmail.com>
2021-10-14 23:24:56 -0600

    updating shader files, to add shader texture

--- docs/js/shaders/vertexShader.js
+++ docs/js/shaders/vertexShader.js
@@ -1,6 +1,15 @@
 export default `
 uniform float delta;
 
+uniform sampler2D noiseTexture;
+uniform float noiseScale;
+
+uniform sampler2D bumpTexture;
+uniform float bumpSpeed;
+uniform float bumpScale;
+uniform float u_time;
+varying vec2 vUv;
+
 void main() {
     vec4 modelViewPosition = modelViewMatrix * vec4(position, 1.0);
     gl_Position = projectionMatrix * modelViewPosition;
