78fbd3497d20f4c5ed7c070ba25e4d5d091223f3
Julio Del Valle <juulio@gmail.com>
2024-08-20 12:00:11 -0600

    Adding content to the BlockKarate component

--- src/BlockKarate.tsx
+++ src/BlockKarate.tsx
@@ -3,9 +3,28 @@ import './BlockKarate.css'
 export default function BlockKarate() {
   return (
     <>
-      <h2>I'm a Karate Teacher</h2>
+      <h2>I'm a Black Belt Karate Teacher</h2>
 
-      <p>Been practicing Kyokushin Karate since 1995.</p>
+      <p>
+        I've been practicing Kyokushin Karate since 1995. I've known hundreds of
+        people while practicing, learning, competing, teaching and sharing a
+        good time through karate activities.
+      </p>
+      <p>
+        In 2004 I was lucky enough to fully dedicate myself to the practice of
+        karate. I was part of an uchi deshi program (internship student) at New
+        York City for 1 month and then at Ikebukuro, Tokyo for 4 months.
+        Needless to say, these experiences had a positive impact on my entire
+        life.
+      </p>
+      <p>
+        I participated in 4 Kyokushin World Tournaments in Japan, in the years
+        2003, 2007, 2009 and 2011.
+      </p>
+      <p>
+        Since 2018 I hold a 3rd degree (san dan) black belt and I'm a teacher in
+        Costa Rica.{' '}
+      </p>
     </>
   )
 }
