74a98869e6919480008942114e74ebf16d6796df
Julio Del Valle <juulio@gmail.com>
2024-08-20 10:57:26 -0600

    Creating footer module, moving social media icons from header to footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,9 +1,34 @@
 import './Footer.css'
+import xLogo from './assets/icon-x.png'
+import githubLogo from './assets/icon-github.png'
+import linkedinLogo from './assets/icon-linkedin.png'
+import youtubeLogo from './assets/icon-youtube.svg'
 
 export default function BlockHome() {
   return (
     <footer>
-      <h3>Developed in Costa Rica - 2024 </h3>
+      <ul className="socialNav">
+        <li>
+          <a href="https://x.com/juulio" target="_blank">
+            <img src={xLogo} alt="X" />
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/juulio" target="_blank">
+            <img src={githubLogo} alt="Github" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/juliodelvalle/" target="_blank">
+            <img src={linkedinLogo} alt="LinkedIn" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.youtube.com/@julioDelValle" target="_blank">
+            <img src={youtubeLogo} alt="YouTube" />
+          </a>
+        </li>
+      </ul>
     </footer>
   )
 }
--- src/Nav.tsx
+++ src/Nav.tsx
@@ -1,8 +1,5 @@
 import { Link } from 'react-router-dom'
 import './Nav.css'
-import linkedinLogo from './assets/icon-linkedin.png'
-import githubLogo from './assets/icon-github.png'
-import youtubeLogo from './assets/icon-youtube.svg'
 
 export default function Header() {
   return (
@@ -23,28 +20,7 @@ export default function Header() {
             </li>
           </ul>
         </li>
-        <li>
-          <ul className="socialNav">
-            <li>
-              <a href="https://www.youtube.com/@julioDelValle" target="_blank">
-                <img src={youtubeLogo} alt="React logo" />
-              </a>
-            </li>
-            <li>
-              <a href="https://github.com/juulio" target="_blank">
-                <img src={githubLogo} alt="React logo" />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/juliodelvalle/"
-                target="_blank"
-              >
-                <img src={linkedinLogo} alt="React logo" />
-              </a>
-            </li>
-          </ul>
-        </li>
+        <li></li>
       </ul>
     </nav>
   )
