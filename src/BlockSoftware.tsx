168859f8a39d0fc5ac2b440390c77f183261aaeb
Julio Del Valle <juulio@gmail.com>
2024-08-20 11:54:33 -0600

    adding content to blockSoftware component

--- src/BlockSoftware.tsx
+++ src/BlockSoftware.tsx
@@ -2,13 +2,38 @@ import './BlockSoftware.css'
 
 export default function BlockSoftware() {
   return (
-    <>
+    <div className="blockSoftware">
       <h2>I'm a software developer</h2>
 
       <p>
         My main interest is to develop creative 3D experiences that run on a web
-        browser.
+        browser, mostly using the{' '}
+        <a href="https://threejs.org/" target="_blank">
+          three.js
+        </a>{' '}
+        framework.
       </p>
-    </>
+      <h3>Most recet work experience</h3>
+      <p>I worked for Microsoft as a software engineer for 2 years.</p>
+      <p>
+        I workd for Wunderman Thompson (formerly Possible) for 11 years as a
+        Front End developer.
+      </p>
+
+      <h3>Education</h3>
+      <p>
+        Computers Engineering, Bachelor's Degree. Instituto Tecnologico de Costa
+        Rica.
+      </p>
+      <p>
+        Three.js Journey Complete Course. Check my certificate at{' '}
+        <a
+          href="https://threejs-journey.com/certificate/view/38336"
+          target="_blank"
+        >
+          https://threejs-journey.com/
+        </a>
+      </p>
+    </div>
   )
 }
