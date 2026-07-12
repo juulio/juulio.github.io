1f191106b97dc70d54fc0675a603ee2df168bb48
Julio Del Valle <juulio@gmail.com>
2026-07-11 23:26:29 -0600

    Update main header

--- src/BlockHome.tsx
+++ src/BlockHome.tsx
@@ -3,11 +3,14 @@ import './BlockHome.css'
 export default function BlockHome() {
   return (
     <>
-      <h2>I'm Julio</h2>
+      <h2>Julio Del Valle | Software Engineer</h2>
 
       <ul>
+        <li>
+          I create high-performance immersive web experiences designed for
+          scalability and user engagement.
+        </li>
         <li>I'm from Costa Rica</li>
-        <li>I'm a Software Developer</li>
         <li>I'm a Karate Teacher</li>
       </ul>
     </>
